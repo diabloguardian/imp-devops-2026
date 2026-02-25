5ab11c4cf7af68718e1d2f7c1e5149ea44f03c97
Dennys <dennysoliver@msn.com>
2026-02-25 15:34:49 -0600

    update output text

--- simple-app-py/simpleapp.py
+++ simple-app-py/simpleapp.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Hello Dennys!</h1>"
+    return "<h1>Hello Dennys Medina!</h1>"
 
 if __name__ == '__main__':
     # Run the app server
