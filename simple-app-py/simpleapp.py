3d8a233d3b766b41880ef281357a086c0fd15586
Dennys <dennysoliver@msn.com>
2026-02-25 01:25:50 -0600

    update app with dependencies && update Dockerfile

--- simple-app-py/simpleapp.py
+++ simple-app-py/simpleapp.py
@@ -1,1 +1,12 @@
-print("Hello from simple app without dependencies!")
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "<h1>Hello Dennys!</h1>"
+
+if __name__ == '__main__':
+    # Run the app server
+    app.run(host='0.0.0.0', port=5000, debug=True)
