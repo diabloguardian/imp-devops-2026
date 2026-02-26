732fb289f65c82ea3e6dbafd483ec538165625a9
Dennys <dennysoliver@msn.com>
2026-02-25 22:11:15 -0600

    Dockerfile multistage && app updated for print datetime

--- simple-app-py/simpleapp.py
+++ simple-app-py/simpleapp.py
@@ -1,11 +1,15 @@
 
+from datetime import datetime
 from flask import Flask
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Hello Dennys Medina!</h1>"
+    current_datetime = datetime.now()
+    formated_datetime = current_datetime.strftime("%d-%m-%Y %H:%M:%S")
+    return f'<h1>Hello Dennys! time is: {formated_datetime}</h1>'
 
 if __name__ == '__main__':
     # Run the app server
